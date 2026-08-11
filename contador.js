c184a84f718097322387f9f41a9ecb61e209016c
joaoemanuel4896-lang <joaoemanuel4896@gmail.com>
2026-08-10 21:17:33 -0500

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,1 +1,54 @@
+// =========================
+// CONTADOR DO RELACIONAMENTO ❤️
+// =========================
 
+// Data em que vocês começaram a namorar
+const inicio = new Date("2026-06-01T21:37:00");
+
+function atualizarContador() {
+
+    const agora = new Date();
+
+    const diferenca = agora - inicio;
+
+    // Evita números negativos
+    if (diferenca < 0) {
+        return;
+    }
+
+    const segundosTotais = Math.floor(diferenca / 1000);
+
+    const dias = Math.floor(
+        segundosTotais / 86400
+    );
+
+    const horas = Math.floor(
+        (segundosTotais % 86400) / 3600
+    );
+
+    const minutos = Math.floor(
+        (segundosTotais % 3600) / 60
+    );
+
+    const segundos = segundosTotais % 60;
+
+
+    document.getElementById("dias").textContent =
+        dias;
+
+    document.getElementById("horas").textContent =
+        horas;
+
+    document.getElementById("minutos").textContent =
+        minutos;
+
+    document.getElementById("segundos").textContent =
+        segundos;
+}
+
+
+// Atualiza imediatamente
+atualizarContador();
+
+// Atualiza a cada segundo
+setInterval(atualizarContador, 1000);
